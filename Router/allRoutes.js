48be27b0af9dfd189a3c61bee48a323e46b10e96
Badreddine <manoudb@yahoo.fr>
2024-08-28 20:34:17 +0200

    navbar

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,12 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Home", "/pages/home.html"),
+    new Route("/Convoiturages", "Convoiturages", "/pages/Covoiturages.html"),
+    new Route("/Connexion", "Connexion", "/pages/Connexion.html"),
+    new Route("/Contact", "Contact", "/pages/Contact.html"),
+
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "EcoRide";
